fcd9c8a197082d6a9d91cb89fd8241457283db64
Mahir KOÇER <mahirkocer@hotmail.com>
2023-05-20 13:25:52 +0300

    beforeEach class added

--- src/test/java/com/cydeo/tests/CalculatorOperationsTest.java
+++ src/test/java/com/cydeo/tests/CalculatorOperationsTest.java
@@ -1,4 +1,40 @@
 package com.cydeo.tests;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class CalculatorOperationsTest {
+
+    AppiumDriver<MobileElement> driver;
+
+    @BeforeEach
+    @Test
+    public void init() throws MalformedURLException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 3");
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10");
+        caps.setCapability(MobileCapabilityType.APP,"https://cybertek-appium.s3.amazonaws.com/calculator.apk");
+
+        URL url = new URL("http://localhost:4723/wd/hub");
+
+        driver = new AndroidDriver<MobileElement>(url,caps);
+    }
+     @AfterEach
+     @Test
+    public void close(){
+
+        driver.closeApp();
+
+     }
 }
